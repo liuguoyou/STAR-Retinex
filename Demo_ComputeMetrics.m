4aceb0d407e3db2069437c3f1d083a7c2f79466b
csjunxu <nankaimathxujun@gmail.com>
2019-01-15 20:10:22 +0400

    add

--- Demo_ComputeMetrics.m
+++ Demo_ComputeMetrics.m
@@ -35,7 +35,7 @@ for d = 1:length(datasets)
     % write_mat_dir = '/home/csjunxu/Paper/Enhancement/Results_NASA/';
     
     %%% begin
-    for m = 10:length(methods)
+    for m = 11:length(methods)
         method = methods{m};
         if strcmp(method, 'None') == 1
             Enhance_dir = Test_dir;
