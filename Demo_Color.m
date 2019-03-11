424e8107d6857e18911339b6a318471ed7071289
XuJun <nankaimathxujun@gmail.com>
2019-03-11 20:52:50 +0400

    add

--- Demo_Color.m
+++ Demo_Color.m
@@ -17,6 +17,25 @@ im_num = length(im_dir);
 i=2;
 name = regexp(im_dir(i).name, '\.', 'split');
 Im=im2double( imread(fullfile('Color', im_dir(i).name)) );
+
+% STAR
+method = 'STAR';
+alpha = 0.001;
+beta = 0.0001;
+ccIm = zeros(size(Im));
+for pI = [1.5]
+    for pR = [0.5]
+        
+        for ch = 1:size(Im,3)
+            S = Im(:,:,ch);
+            [I, R] = STAR(Im, alpha, beta, pI, pR);
+            ccIm(:,:,ch) = R;
+        end
+        imwrite(ccIm(hsv), [name{1} '_' method '.png']);
+    end
+end
+
+
 % WVM CVPR2016
 method = 'WVM';
 Im = 255*Im;
@@ -28,8 +47,8 @@ for ch = 1:size(Im,3)
     [ R, I, epsilon_R, epsilon_L ] = WVM_CVPR2016( S, c_1, c_2, lambda, epsilon_stop );
     ccIm(:,:,ch) = R;
 end
-    imwrite(ccIm, [name{1} '_' method '.png'])
-    
+imwrite(ccIm, [name{1} '_' method '.png']);
+
 % JIEP ICCV2017
 method = 'JieP';
 [I, R] = jiep(Im);
@@ -45,24 +64,3 @@ hsv(:,:,3) = R;
 subplot(2,2,4); imshow(hsv2rgb(hsv));  title('Reflectance (RGB)');
 imwrite(hsv2rgb(hsv), [write_dir name{1} '_R_RGB_' method '.png'])
 %subplot(2,2,1); imshow(Im);  title('Input');
-
-% STAR
-method = 'STAR';
-alpha = 0.001;
-beta = 0.0001;
-for pI = [1.5:.1:2]
-    for pR = [0.5:-.1:.1]
-        [I, R] = STAR(Im, alpha, beta, pI, pR);
-        hsv = rgb2hsv(Im);
-        subplot(2,2,1); imshow(I);  title('Illumination (Gray)');
-        imwrite(I, [write_dir name{1} '_I_Gray_' method '_pI' num2str(pI) '_pR' num2str(pR) '.png'])
-        hsv(:,:,3) = I;
-        subplot(2,2,2); imshow(hsv2rgb(hsv));  title('Illumination (RGB)');
-        imwrite(hsv2rgb(hsv), [write_dir name{1} '_I_RGB_' method '_pI' num2str(pI) '_pR' num2str(pR) '.png'])
-        subplot(2,2,3); imshow(I);  title('Reflectance (Gray)');
-        imwrite(R, [write_dir name{1} '_R_Gray_' method '_pI' num2str(pI) '_pR' num2str(pR) '.png'])
-        hsv(:,:,3) = R;
-        subplot(2,2,4); imshow(hsv2rgb(hsv));  title('Reflectance (RGB)');
-        imwrite(hsv2rgb(hsv), [write_dir name{1} '_R_RGB_' method '_pI' num2str(pI) '_pR' num2str(pR) '.png'])
-    end
-end
