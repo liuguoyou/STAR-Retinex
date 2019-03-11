89a7be92de22929c314b7c0df7fcaa7a9fe653db
XuJun <nankaimathxujun@gmail.com>
2019-03-11 21:33:16 +0400

    add

--- Demo_Color.m
+++ Demo_Color.m
@@ -18,6 +18,16 @@ i=2;
 name = regexp(im_dir(i).name, '\.', 'split');
 Im=im2double( imread(fullfile('Color', im_dir(i).name)) );
 
+% JIEP ICCV2017
+method = 'JieP';
+ccIm = zeros(size(Im));
+for ch = 1:size(Im,3)
+    S = Im(:,:,ch);
+    [I, R] = jiep(S);
+    ccIm(:,:,ch) = R;
+end
+imwrite(ccIm, [name{1} '_' method '.png']);
+
 % STAR
 method = 'STAR';
 alpha = 0.001;
@@ -25,7 +35,6 @@ beta = 0.0001;
 ccIm = zeros(size(Im));
 for pI = [1.5]
     for pR = [0.5]
-        
         for ch = 1:size(Im,3)
             S = Im(:,:,ch);
             [I, R] = STAR(Im, alpha, beta, pI, pR);
@@ -48,19 +57,3 @@ for ch = 1:size(Im,3)
     ccIm(:,:,ch) = R;
 end
 imwrite(ccIm, [name{1} '_' method '.png']);
-
-% JIEP ICCV2017
-method = 'JieP';
-[I, R] = jiep(Im);
-hsv = rgb2hsv(Im);
-subplot(2,2,1); imshow(I);  title('Illumination (Gray)');
-imwrite(I, [write_dir name{1} '_I_Gray_' method '.png'])
-hsv(:,:,3) = I;
-subplot(2,2,2); imshow(hsv2rgb(hsv));  title('Illumination (RGB)');
-imwrite(hsv2rgb(hsv), [write_dir name{1} '_I_RGB_' method '.png'])
-subplot(2,2,3); imshow(I);  title('Reflectance (Gray)');
-imwrite(R, [write_dir name{1} '_R_Gray_' method '.png'])
-hsv(:,:,3) = R;
-subplot(2,2,4); imshow(hsv2rgb(hsv));  title('Reflectance (RGB)');
-imwrite(hsv2rgb(hsv), [write_dir name{1} '_R_RGB_' method '.png'])
-%subplot(2,2,1); imshow(Im);  title('Input');
